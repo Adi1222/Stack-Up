80fd8177ee52aa217e327de8a3e412b3ae444ece
Aditya Chavan <adityachavan677@gmail.com>
2021-01-02 20:40:56 +0530

    added routes for answers

---
+++ server/routes/answers.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const router = express.Router();
+const { check } = require('express-validator')
+
+const {
+    getAnswers,
+    addAnswer,
+    deleteAnswer
+} = require('../controllers/answers')
+
+
+/**
+ *   GET /answers/:qid
+ *   Fetch all the answers for a particular question
+ *   Private access
+ * 
+ */
+ router.get('/:qid', getAnswers)
+
+
+
+
+ /**
+  *   POST /answers/:id
+  *   Add an answer to a question 
+  *   Private
+  */  
+  router.post('/:qid', addAnswer)
+
+
+
+/**
+ *   DELETE /answers/:qid/:aid
+ *   delete an answer of a particular question
+ *   private access
+ */
+  router.delete('/:qid/:aid', deleteAnswer)
+
+
+
+
+module.exports = router;

9726f2c84c1d34d5a38fbd41923410cfc0eaefc8
Aditya Chavan <adityachavan677@gmail.com>
2021-01-02 20:40:28 +0530

    index routes

---
+++ server/routes/index.route.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router();
+
+
+const questions = require('./questions')
+const answers = require('./answers')
+
+router.use('/questions', questions)
+router.use('/answers', answers)
+
+module.exports = router;

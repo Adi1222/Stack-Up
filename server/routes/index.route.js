aa14f66acb4ecf8131785dbde7703b288fe2a776
Aditya Chavan <adityachavan677@gmail.com>
2021-01-03 20:20:58 +0530

    users routes added

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,5 +1,6 @@
 const questions = require('./questions')
 const answers = require('./answers')
+const users = require('./users')
 
 const express = require('express')
 const router = express.Router();
@@ -9,5 +10,6 @@ const router = express.Router();
 
 router.use('/questions', questions)
 router.use('/answers', answers)
+router.use('/users/', users)
 
 module.exports = router;

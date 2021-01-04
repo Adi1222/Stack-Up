ddc663bca622f1035b50dbbd2702962357672531
Aditya Chavan <adityachavan677@gmail.com>
2021-01-04 19:54:04 +0530

    comments route

---
+++ server/routes/comments.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const router = express.Router();
+
+
+const {
+
+    deleteComment, addComment
+
+} = require('../controllers/comments')
+
+
+
+
+/**
+ *  POST /comments/:question/:answer?
+ *  add a comment to a question or to an answer
+ * 
+ */
+ router.post('/:question/:answer?', addComment)
+
+
+
+
+
+
+/**
+ *    DELETE /comments/:question/:comment
+ *    delete a comment of a particular question
+ *   Private access 
+ */   
+ router.delete('/:question/:comment', deleteComment);
+
+
+  
+ /**
+  *   DELETE /comments/:question/:answer/:comment
+  *   Deleting a comment of a particular  answer of a particluar question 
+  */
+  router.delete('/:question/:answer/:comment', deleteComment);
+
+
+
+module.exports = router;

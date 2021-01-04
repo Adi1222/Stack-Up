f8a59ef38ff2baff516f9c8614a72b2d2442b0d3
Aditya Chavan <adityachavan677@gmail.com>
2021-01-04 19:52:15 +0530

    comments route added

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -2,6 +2,7 @@ const questions = require('./questions')
 const answers = require('./answers')
 const users = require('./users')
 const tags = require('./tags')
+const comments = require('./comments')
 
 const express = require('express')
 const router = express.Router();
@@ -14,5 +15,6 @@ router.use('/questions', questions)
 router.use('/answers', answers)
 router.use('/users/', users)
 router.use('/tags/', tags)
+router.use('/comments/', comments)
 
 module.exports = router;

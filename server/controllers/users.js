84b6fd836f618b769aab771171b1066f8bf9836c
Aditya Chavan <adityachavan677@gmail.com>
2021-01-07 21:05:13 +0530

    login and signup done

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,4 +1,113 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+
+
+const signup = async (req, res) => {
+
+    try {
+
+        const username = req.body.username;
+
+        const pass = req.body.password;
+
+        const userObect = {
+            username: username.toLowerCase(),
+            password: pass
+        };
+
+        const existingUser = await User.findOne({ username: u.username });
+
+        if (existingUser) 
+        {
+            return res.status(400).json({ message: 'Username Already Exists.' })
+        }
+
+
+        const u = new User(userObect);
+        const savedUser = await u.save();
+
+        if (savedUser)
+        {
+            const token = jwt.sign(savedUser, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5d' })
+
+            const { username, id, created, profilepic } = savedUser;
+
+            const userInfo = {
+                username,
+                id, 
+                created, 
+                profilepic
+            }
+
+
+            return res.json({ message: 'User Created', token, userInfo })
+
+        }
+        else
+        {
+            return res.json(400).json({ message: 'Cannot create your Account!' })
+        }
+
+
+
+        
+    } catch(error) {
+
+        console.error(error)
+        return res.json(400).json({ message: 'Cannot create your Account!' })
+
+    } 
+
+
+} 
+
+const login = async (req, res) => {
+
+    try {
+        
+        const username = req.body.username;
+        const password = req.body.password;
+
+        const user = await User.findOne({ username: username.toLowerCase() })
+
+        if (!user) 
+        {
+            return res.status(403).json({ message: 'Invalid username or password' }) 
+        }
+
+        const match = await bcrypt.compare(password, user.password);
+
+        if (match) 
+        {
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '5d' })
+            
+            const { username, id, created, profilepic } = savedUser;
+
+            const userInfo = {
+                username,
+                id, 
+                created, 
+                profilepic
+            }
+
+
+            return res.json({ message: 'Authentication Successfull', token, userInfo })
+        }
+        else 
+        {
+            res.status(403).json({
+              message: 'Wrong username or password.'
+            })
+        }
+
+    } catch(error) {
+        console.error(error)
+        return res.json(400).json({ message: 'ERROR!' })   
+    }
+
+
+}
 
 const getUsers =  async (req, res) => {
 
@@ -33,5 +142,7 @@ const getSingleUser = async (req, res) => {
 
 module.exports = {
     getUsers,
-    getSingleUser
+    getSingleUser,
+    signup,
+    login
 }

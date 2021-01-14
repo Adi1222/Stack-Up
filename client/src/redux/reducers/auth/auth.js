deaf81a3496a0507a30b00be79c8d39f5e103d85
Aditya Chavan <adityachavan677@gmail.com>
2021-01-14 13:35:42 +0530

    auth reducers

--- client/src/redux/reducers/auth/auth.js
+++ client/src/redux/reducers/auth/auth.js
@@ -1,3 +1,6 @@
+import * as authTypes from '../../actions/auth/auth.types';
+
+
 const initialState = {
     token: null,
     loading: true,
@@ -9,8 +12,48 @@ const initialState = {
 
 const authReducer = (state = initialState, action) => {
     switch(action.type) {
+        
+        case authTypes.LOGIN_SUCCESS:
+            const { data } = action.payload;
+            const { token, userInfo } = data;
+            localStorage.setItem('token', token);
+            localStorage.setItem('userInfo', JSON.stringify(userInfo))
+            return {
+                ...state,
+                ...action.payload,
+                isAuthenticated: true,
+                loading: false
+            };
+
+        case authTypes.REGISTER_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+                isAuthenticated: false
+            };
+
+        case authTypes.LOGIN_FAIL:
+            return {
+                ...state,
+                loading: false,
+                error: action.payload,
+                isAuthenticated: false
+            }
+        
+        case authTypes.LOGOUT:
+            localStorage.removeItem("token");
+            localStorage.removeItem("userInfo");
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            };
+        
         default:
             return state;
+
     }
 } 
 

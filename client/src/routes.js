858c4fe89b7f887aa7315d0339c54407f2d4c0a0
Aditya Chavan <adityachavan677@gmail.com>
2021-01-15 22:48:48 +0530

    added route for signup

--- client/src/routes.js
+++ client/src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter ,Switch, Route } from 'react-router-dom';
 
 import Login from './pages/Login/Login.component';
+import SignupComponent from './pages/Register/Signup.component';
 
 const Routes = () => {
     return( 
     <BrowserRouter>
         <Switch>    
             <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignupComponent} />
         </Switch>
     </BrowserRouter>
     )

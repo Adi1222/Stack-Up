ff8cdee25d1989b3d736e07c0c0686b2fd8df829
Aditya Chavan <adityachavan677@gmail.com>
2021-01-16 23:04:23 +0530

    resolved the homepage error

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux';
 import './App.css';
 import store from './redux/store';
 import Routes from './routes';
+import Header from './components/Header/Header.component'
 
 const App = () => {
   return (

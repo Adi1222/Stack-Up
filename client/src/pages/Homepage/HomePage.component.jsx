20fd22a21c066c1c5fbdbd44e0e72fe22f1e812d
Aditya Chavan <adityachavan677@gmail.com>
2021-01-17 00:33:14 +0530

    Homepage remaining

---
+++ client/src/pages/Homepage/HomePage.component.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import Header from '../../components/Header/Header.component'
+
+const HomePage = () => {
+
+    return(
+        <Fragment>
+            <Header />
+        </Fragment>
+    )
+
+}
+
+export default HomePage;

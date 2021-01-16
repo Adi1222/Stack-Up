bd387aa4b1a8278d1dbfd1eb854225f6bb30c25e
Aditya Chavan <adityachavan677@gmail.com>
2021-01-16 23:04:59 +0530

    homepage route added

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { BrowserRouter ,Switch, Route } from 'react-router-dom';
+import HomePage from './pages/Homepage/HomePage.component';
 
 import Login from './pages/Login/Login.component';
 import SignupComponent from './pages/Register/Signup.component';
 
+
+
 const Routes = () => {
     return( 
     <BrowserRouter>
-        <Switch>    
+        <Switch>
+            <Route exact path="/" component={HomePage} />  
+            <Route exact path="/signup" component={SignupComponent} />  
             <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={SignupComponent} />
         </Switch>
     </BrowserRouter>
     )

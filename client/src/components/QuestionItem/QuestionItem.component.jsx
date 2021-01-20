94a87c583604045753f0bb79156aa69eafe9e7e0
Aditya Chavan <adityachavan677@gmail.com>
2021-01-20 15:20:02 +0530

    added QuestionItem component (styling remaining)

---
+++ client/src/components/QuestionItem/QuestionItem.component.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const QuestionItem = ({ question: { id, title, body, tags, answers, comments, created_at, views, author } }) => {
+
+    return (
+        <div>
+            <div>
+                <div>
+                    <span>0</span>
+                    <p>votes</p>
+                </div>
+                
+                <div>
+                    <span>{answers.length}</span>
+                    <p>answers</p>
+                </div>
+                
+                <div>
+                    <span>{views}</span>
+                    <p>vies</p>
+                </div>
+            </div>
+
+            <div> 
+                <h1><Link to={`/questions/${id}`}>{title}</Link></h1>
+                <div>
+                    {body}
+                </div>
+                <div>
+                    <Link to={`/users/${author.id}`} title={author.username}>
+                        <img alt='user-logo' className='userlogo'
+                        src={`https://secure.gravatar.com/avatar/${author.id}?s=164&d=identicon`}/>
+                    </Link>
+                </div>
+                <div>
+                    <span>
+                        asked{' '}
+                        {
+                            new Date(created_at)
+                        } 
+                    </span>
+                    <Link to={`/users/${author.id}`}>
+                        {author.username}
+                    </Link>
+                </div>
+            </div>
+
+
+        </div>
+    )
+
+}
+
+
+
+export default QuestionItem;

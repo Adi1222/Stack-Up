fc8a599cdbce0edb2a77ad2cbe076328bf41b350
Aditya Chavan <adityachavan677@gmail.com>
2021-01-20 13:16:36 +0530

    added question reducer to rootreducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import authReducer from '../reducers/auth/auth';
+import questionReducer from '../reducers/questions/questions';
 
 const rootReducer = combineReducers({
     auth: authReducer,
+    quest: questionReducer
 })
 
 export default rootReducer;

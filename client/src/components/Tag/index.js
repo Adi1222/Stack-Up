38013dea9bff1a3fb726459ab4cac8afce3cae21
Aditya Chavan <adityachavan677@gmail.com>
2021-01-24 14:26:03 +0530

    Tag component

---
+++ client/src/components/Tag/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+import LinkButton from '../LinkButton/LinkButton.component';
+import Button from '@material-ui/core/Button';
+ 
+const Tag = ({ children }) => {
+
+    const history = useHistory();
+
+    const routeChange = (tag) => {
+        let path = `/${tag}`;
+        history.push(path);
+    }
+
+
+
+    return (
+        <Button variant="contained" color="primary" onClick={routeChange(children)} >
+            {children}
+        </Button>
+    )
+}
+
+export default Tag;
